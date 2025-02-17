efd4d3bba8a93fe221927297c4f88365b263e6f5
Marina Kokorovets <mkokorovetz@gmail.com>
2025-02-17 13:50:01 +0300

    update style.css

--- 3/form.php
+++ 3/form.php
@@ -1,4 +1,4 @@
-<form action="" method="POST">
+<form id="form" action="" method="POST">
   <input name="fio" type="text" placeholder="ФИО"><br>
   <input name="telephone" type="tel" placeholder="Номер телефона"><br>
   <input name="email" type="email" placeholder="Email@mail.mail"><br>
@@ -7,13 +7,13 @@
   <input name="dateOfBirth" type="date"><br>
 
   Пол:<br>
-  <label><input name="radio" checked="checked" type="radio" value="female">Женский</label>
-  <label><input name="radio" type="radio" value="male">Мужской</label><br>
+  <label><input class="radio" name="radio" checked="checked" type="radio" value="female">Женский</label>
+  <label><input class="radio" name="radio" type="radio" value="male">Мужской</label><br>
 
   
   Любимый язык программирования:
   <br>
-  <select name="abilities[]" multiple="multiple">
+  <select id="abilities" name="abilities[]" multiple="multiple">
       <?php 
       foreach ($abilities as $key => $value) {
         printf('<option value="%s">%s</option>', $key, $value);
@@ -24,10 +24,10 @@
 
   
   Биография:<br>
-  <textarea name="bio"></textarea>
+  <textarea id="bio" name="bio"></textarea>
   <br>
 
-  <input name="check" type="checkbox" checked="checked">С контрактом ознакомлен(а)<br>
+  <input id="check" name="check" type="checkbox" checked="checked">С контрактом ознакомлен(а)<br>
 
-  <input name="send" type="submit" value="Сохранить">
+  <input id="submit" name="send" type="submit" value="Сохранить">
 </form>
